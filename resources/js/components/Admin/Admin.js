d540e1b8b619de8141678c6275781777d8885baf
quocviet0706 <nguyenquocviet0706@gmail.com>
2021-11-12 20:45:13 +0700

    update design

--- resources/js/components/Admin/Admin.js
+++ resources/js/components/Admin/Admin.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Main from "../Main";
 import ProductManager from "./ProductManager/ProductManager";
-import UserManager from "./UserManager/UserManager";
-// import CreateUser from "./UserManager/CreateUser";
 
 
 export default function Admin() {
     return (
         <div className="admin">
-            < UserManager/>
+            < ProductManager/>
         </div>
     );
 }
---
+++ resources/js/components/Admin/User.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UserManager from "./UserManager/UserManager";
+
+export default function User() {
+    return (
+        <div className="user">
+            < UserManager/>
+        </div>
+    );
+}
